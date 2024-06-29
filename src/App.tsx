4fbc387b1fd1cc0fd62b7e590e483fe4fde1a842
Megan <smith.megan.design@gmail.com>
2024-06-28 23:01:46 -0700

    add custom css colors and fonts, fix bug with import, incomplete json export of data

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { format, getDaysInMonth } from "date-fns"
 import soundList from "./assets/sounds/sounds"
 import "./App.css"
-import DownloadJSON from "./Components/download"
+import DownloadJSON from "./Components/download.tsx"
 
 function App() {
   interface HistoryStructure {
@@ -46,34 +46,36 @@ function App() {
 
   return (
     <>
-      <div
-        className={
-          "grid grid-flow-row font-semibold text-lg p-10 text-orange-200"
-        }
-      >
-        {date}
-      </div>
-      <div className="grid grid-cols-5 gap-7">
-        {Object.keys(history).map((item: any) => {
-          return (
-            <div key={"button-" + item}>
-              <button
-                className={
-                  "bg-gradient-to-tl " +
-                  history[item] +
-                  " border-2 border-orange-300 rounded-full p-1 w-16 h-16 text-center text-gray-700 shadow-inner focus:border-orange-400 active:border-orange-400 hover:border-orange-400"
-                }
-                onClick={() => {
-                  changeColor(item)
-                  playSound()
-                  DownloadJSON(history, date)
-                }}
-              >
-                {item}
-              </button>
-            </div>
-          )
-        })}
+      <div className="grid justify-self-center bg-burgundy rounded-lg p-10 mt-11 w-3/4">
+        <div
+          className={
+            "grid grid-flow-row font-semibold text-4xl p-10 text-orange-200 font-header"
+          }
+        >
+          {date}
+        </div>
+        <div className="grid grid-cols-7 gap-7">
+          {Object.keys(history).map((item: any) => {
+            return (
+              <div key={"button-" + item}>
+                <button
+                  className={
+                    "bg-gradient-to-tl " +
+                    history[item] +
+                    " border-2 border-orange-300 rounded-full p-1 w-16 h-16 text-center text-gray-700 shadow-inner focus:border-orange-400 active:border-orange-400 hover:border-orange-400"
+                  }
+                  onClick={() => {
+                    changeColor(item)
+                    playSound()
+                    DownloadJSON(history, date)
+                  }}
+                >
+                  {item}
+                </button>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { burgundy: "rgb(40, 16, 16)" },
+      fontFamily: {
+        "font-header": ["Dosis", '"sans-serif"'],
+      },
+    },
   },
   plugins: [],
 }
